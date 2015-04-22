4be73bd3ae18aecd8e707efe3f775c2a2287111c
flxmdy <flxmdy@gmail.com>
2015-04-22 18:12:21 -0400

    Tried to add malloc error handling, but not working

--- cudaErr.h
+++ cudaErr.h
@@ -1,5 +1,5 @@
 /*
- * Cuda and CuBLAS error handers and macro wrappers
+ * Allocation, Cuda and CuBLAS error handers and macro wrappers
  */
 
 #ifndef cudaErr
@@ -10,22 +10,43 @@
 #include <cuda_runtime.h>
 #include "cublas_v2.h"
 
-// cuda and cublas error handlers wrappers
+// error handlers wrappers
+#define allocErrChk(err) { allocErrorHandler(err, __FILE__, __LINE__); }
 #define cudaErrChk(err) { cudaErrorHandler(err, __FILE__, __LINE__); }
-#define cublasErrChk(err) {cublasErrorHandler(err, __FILE__, __LINE__); }
+#define cublasErrChk(err) { cublasErrorHandler(err, __FILE__, __LINE__); }
 
-// cuda and cublas error handlers
-inline void cudaErrorHandler(cudaError_t err, const char * file, int line) {
-    if (err != cudaSuccess) {
-        fprintf(stderr, "CUDA Error in %s on line %d:%s\n", file, line, cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
+// error handlers
+inline void * allocErrorHandler(void * return_ptr, const char * file, int line) {
+	if (return_ptr == NULL) {
+		fprintf(stderr,
+				"Allocation error in %s on line %d\n",
+				file,
+				line);
+		exit(EXIT_FAILURE);
+	}
+	return return_ptr;
 }
+
+inline void cudaErrorHandler(cudaError_t err, const char * file, int line) {
+	if (err != cudaSuccess) {
+		fprintf(stderr,
+				"CUDA Error in %s on line %d:%s\n",
+				file, 
+				line,
+				cudaGetErrorString(err));
+		exit(EXIT_FAILURE);
+	}
+}
+
 inline void cublasErrorHandler(cublasStatus_t err, const char * file, int line) {
-    if (err != CUBLAS_STATUS_SUCCESS) {
-        fprintf(stderr, "CUBLAS Error in %s on line $d:%s\n", file, line, (const char *)err);
-        exit(EXIT_FAILURE);
-    }
+	if (err != CUBLAS_STATUS_SUCCESS) {
+		fprintf(stderr,
+				"CUBLAS Error in %s on line $d:%s\n",
+				file,
+				line,
+				(const char *)err);
+		exit(EXIT_FAILURE);
+	}
 }
 
 #endif
