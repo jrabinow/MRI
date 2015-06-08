1a61ebf30f53c09b1e64c4b7735a5e5664dccc36
Julien Rabinow <jnr305@nyu.edu>
2015-06-08 14:11:53 -0400

    Cleaned up repository

--- tests/launchGrasp.sh
+++ tests/launchGrasp.sh
@@ -8,6 +8,9 @@ elif [ $# -ge 1 ] && [ $1 == '-v' ]; then
 elif [ $# -ge 1 ] && [ $1 == '-m' ]; then
 	shift
 	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/pkg/cuda/6.5/lib64:./gpuNUFFT-2.0.6rc2/CUDA/bin cuda-memcheck "$@" ./grasp
+elif [ $# -ge 1 ] && [ $1 == '-t' ]; then
+	shift
+	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/pkg/cuda/6.5/lib64:./gpuNUFFT-2.0.6rc2/CUDA/bin time "$@" ./grasp
 else
 	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/pkg/cuda/6.5/lib64:./gpuNUFFT-2.0.6rc2/CUDA/bin ./grasp
 fi
